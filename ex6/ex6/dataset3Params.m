ff8662698e287c902d20544b8193e7ae2848cf24
Og192 <og192liu@gmail.com>
2016-06-07 10:47:14 +0800

    ex6 svm code

--- ex6/ex6/dataset3Params.m
+++ ex6/ex6/dataset3Params.m
@@ -22,8 +22,28 @@ sigma = 0.3;
 %  Note: You can compute the prediction error using 
 %        mean(double(predictions ~= yval))
 %
+Cs = [0.01, 0.03, 0.1, 0.3, 1, 3, 10, 30];
+sigmas = [0.01, 0.03, 0.1, 0.3, 1, 3, 10, 30];
+tmpC = 0;
+tmpsigma = 0;
+error = 1;
+for i=1:8
+    for j = 1:8
+        tmpC = Cs(i);
+        tmpsigma = sigmas(j);
 
+        x1 = [1 2 1]; x2 = [0 4 -1];
+        model= svmTrain(X, y, tmpC, @(x1, x2) gaussianKernel(x1, x2, tmpsigma));
+        predictions = svmPredict(model, Xval);
+        tmperror = mean(double(predictions ~= yval));
+        if(tmperror < error)
+            error = tmperror;
+            C = tmpC;
+            sigma=tmpsigma;
+        end
 
+    end
+end
 
 
 
--- ex6/ex6/processEmail.m
+++ ex6/ex6/processEmail.m
@@ -97,6 +97,11 @@ while ~isempty(email_contents)
     %       str2). It will return 1 only if the two strings are equivalent.
     %
 
+    for i=1:size(vocabList)(1)
+        if(strcmp(str, vocabList{i}))
+            word_indices =[word_indices; i];
+        end
+    end
 
 
 
